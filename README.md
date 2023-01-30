98884be928f77da80ba8dbbf2f4058fbfcdac9b6
poscat <poscat@poscat.moe>
2023-01-30 23:33:33 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nixos-firewall-ng
+
+An improved zone-based nftables firewall for NixOS that is suitable for both routers and PCs.
---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "An improved zone-based nftables firewall for NixOS that is suitable for both routers and PCs.";
+  outputs =
+    {...}:
+    {
+      nixosModules.nixos-firewall-ng = import ./.;
+    };
+}
